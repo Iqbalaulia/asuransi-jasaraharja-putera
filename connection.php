6313d93d61326d3ac20afa42910273df99ee4e8c
Muhammad Iqbal Aulia Rafi <16410100012@stikom.edu>
2020-01-17 23:01:03 +0700

    chane connection

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php 
-$koneksi = mysqli_connect("localhost","root","sepatusandal12","jasarahaja");
+$koneksi = mysqli_connect("localhost","","","");
  
 // Check connection
 if (mysqli_connect_errno()){
